72ef9ae9c076fc6ef4d02336603e73c8f7ddf0b4
Andrea Duina <andreaduina22@gmail.com>
2024-06-04 16:37:40 +0200

    add github api

--- src/home/pages/Home.vue
+++ src/home/pages/Home.vue
@@ -1,7 +1,34 @@
 <template>
   <div>Homedev</div>
+  <div>{{ res }}</div>
 </template>
 
 <script setup lang="ts">
-  console.log('Home')
+  import { ref } from 'vue'
+  import { getIssues } from '~/common/api/github'
+
+  const res = ref()
+
+  const init = async () => {
+    const result = await getIssues('AndreaDuina')
+    res.value = processEvents(result.data)
+  }
+
+  const processEvents = (events: any[]) => {
+    console.log('events', events)
+    const activity: any = {}
+
+    events.forEach((event: any) => {
+      const date = new Date(event.created_at).toISOString().split('T')[0]
+      if (!activity[date]) {
+        activity[date] = 0
+      }
+      activity[date]++
+    })
+
+    console.log('activity', activity)
+    return activity
+  }
+
+  init()
 </script>
