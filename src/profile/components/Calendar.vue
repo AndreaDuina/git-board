5a994c55e46a80032eb52ebf0e6be0930162c647
Andrea Duina <andreaduina22@gmail.com>
2024-06-05 12:10:22 +0200

    add axis names to calendar

--- src/profile/components/Calendar.vue
+++ src/profile/components/Calendar.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="flex justify-center" v-if="calendar">
+    <div class="inline-block">
+      <div class="flex flex-col" v-for="i of 8">
+        <div class="m-[0.1rem] flex h-4 w-4 items-center justify-center rounded-sm">
+          {{ weekDays[i - 1] }}
+        </div>
+      </div>
+    </div>
     <div class="inline-block" v-for="week of calendar.weeks">
+      <div class="m-[0.1rem] flex h-4 w-4 items-center justify-center rounded-sm">
+        {{ monthTitles[week.firstDay] }}
+      </div>
       <div class="flex flex-col" v-for="day of week.days">
         <div
           class="m-[0.1rem] h-4 w-4 rounded-sm"
@@ -12,11 +22,37 @@
 </template>
 
 <script setup lang="ts">
-  import { PropType } from 'vue'
+  import { PropType, computed } from 'vue'
 
   const props = defineProps({
     calendar: { type: Object as PropType<GitDashboardCalendar>, required: true }
   })
+
+  const weekDays = {
+    0: '',
+    1: 'M',
+    2: '',
+    3: 'W',
+    4: '',
+    5: 'F',
+    6: '',
+    7: 'S'
+  }
+
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
 
   const getContributionColor = (count: number): string => {
     switch (true) {
@@ -32,4 +68,22 @@
         return '#216e39'
     }
   }
+
+  const monthTitles = computed(() => {
+    const result: { [firstDay: string]: string } = {}
+
+    let lastMonth = ''
+    for (const week of props.calendar.weeks) {
+      const firstDay = week.firstDay
+      const month = firstDay.substring(5, 7)
+      if (lastMonth != month) {
+        result[firstDay] = monthNames[parseInt(month) - 1]
+        lastMonth = month
+        continue
+      }
+      result[firstDay] = ''
+    }
+
+    return result
+  })
 </script>
