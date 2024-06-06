cc9acbfffea0982cd9543196b1f963af13ac487e
Andrea Duina <andreaduina22@gmail.com>
2024-06-06 15:41:51 +0200

    setup navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="min-h-screen w-full bg-background text-base">
-    <main class="mx-auto my-0 block max-w-screen-xl p-1">
-      <RouterView v-slot="{ Component }">
-        <template v-if="Component">
-          <component :is="Component" :key="router.currentRoute.value.fullPath" />
-        </template>
-      </RouterView>
-    </main>
+    <div class="mx-auto my-0 block max-w-screen-xl">
+      <Navigation />
+      <main class="mt-4 p-1">
+        <RouterView v-slot="{ Component }">
+          <template v-if="Component">
+            <component :is="Component" :key="router.currentRoute.value.fullPath" />
+          </template>
+        </RouterView>
+      </main>
+    </div>
   </div>
 
   <div class="absolute top-0 left-0 flex flex-col gap-4 bg-red p-2" v-if="isDev">
@@ -21,6 +24,7 @@
 </template>
 
 <script setup lang="ts">
+  import Navigation from '~/common/components/Navigation.vue'
   import { useRouter } from 'vue-router'
 
   const router = useRouter()
--- src/common/components/SearchBar.vue
+++ src/common/components/SearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto mt-8 max-w-7xl px-4 sm:px-6 lg:px-8">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <label for="search" class="sr-only">Search</label>
     <div class="relative">
       <!-- List box -->
