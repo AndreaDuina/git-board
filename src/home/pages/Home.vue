454a12f802490b8bd685426e9307fb341e2ca921
Andrea Duina <andreaduina22@gmail.com>
2024-06-06 17:42:14 +0200

    search from home with state

--- src/home/pages/Home.vue
+++ src/home/pages/Home.vue
@@ -6,6 +6,14 @@
 
 <script setup lang="ts">
   import SearchBar from '~/common/components/SearchBar.vue'
+  import { useStateStore } from '~/stores/state'
+  import { useRouter } from 'vue-router'
 
-  const search = async (username: string, platforms: string[]) => {}
+  const state = useStateStore()
+  const router = useRouter()
+
+  const search = async (username: string, platforms: string[]) => {
+    state.searchUsersParams = { username, platforms }
+    router.push({ name: 'create' })
+  }
 </script>
