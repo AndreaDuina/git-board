1c9e680cfed2c19cd4ddfcfc4504d1d46c3e1a9c
Andrea Duina <andreaduina22@gmail.com>
2024-06-08 12:08:01 +0200

    basic firestore integration

--- src/common/firebase/auth.ts
+++ src/common/firebase/auth.ts
@@ -1,7 +1,10 @@
-import { auth } from '~/common/firebase/firebase'
-import { GithubAuthProvider, signInWithPopup } from 'firebase/auth'
+import { auth, firestore } from '~/common/firebase/firebase'
+import { GithubAuthProvider, signInWithPopup, User } from 'firebase/auth'
+import { collection, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const githubProvider = new GithubAuthProvider()
+
+const usersRef = collection(firestore, 'users')
 
 export const signInWithGitHub = async () => {
   try {
@@ -12,11 +15,30 @@ export const signInWithGitHub = async () => {
     const credential = GithubAuthProvider.credentialFromResult(res)
     const token = credential?.accessToken
     const user = res.user
+
+    await onSignIn(user, token)
   } catch (err: any) {
     const errorCode = err.code
     const errorMessage = err.message
     const email = err.customData.email
     const credential = GithubAuthProvider.credentialFromError(err)
     console.log(`Error ${errorCode} during GitHub authentication for ${email}`, credential)
+  }
+}
+
+const onSignIn = async (user: User, token: string | undefined = undefined) => {
+  // Get user data from db
+  const docSnap = await getDoc(doc(usersRef, user.uid))
+
+  if (docSnap.exists()) {
+    const data = docSnap.data()
+    console.log(data)
+  } else {
+    console.log(`Data for user ${user.uid} not found`)
+    // Add user to db
+    await setDoc(doc(usersRef, user.uid), {
+      name: user.displayName,
+      email: user.email
+    })
   }
 }
